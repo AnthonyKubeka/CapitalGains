81c892ffb5e682a8b085ec0e725da317b22c80e3
Anthony Kubeka <nmkubeka@gmail.com>
2021-03-09 21:26:00 +0200

    Corrected file structure for persistence layer

---
+++ dotNET/WebApi/Persistence/Repositories/LessonCardRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Persistence.Interface;
+namespace Persistence.Repositories
+{
+    public class LessonCardRepository : ILessonCardRepository
+
+    {
+        public LessonCard FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
