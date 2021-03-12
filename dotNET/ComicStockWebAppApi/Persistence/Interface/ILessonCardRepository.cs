0708edd40edd6d9f28f0ae0acfd4e33db35c5823
anthony.kubeka <anthony.kubeka@entelect.co.za>
2021-03-12 16:14:47 +0200

    Added net core version of api

---
+++ dotNET/ComicStockWebAppApi/Persistence/Interface/ILessonCardRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Models; 
+namespace Persistence.Interface
+{
+    public interface ILessonCardRepository
+    {
+
+        LessonCardEntity FindById(int id);
+    }
+}
